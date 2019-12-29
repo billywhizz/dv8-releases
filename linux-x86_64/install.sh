aaa3982608849ae94f8edef62e7f1a3c6e38b744
Andrew Johnston <apjohnsto@gmail.com>
2019-12-29 03:34:52 +0000

    v0.0.9

--- linux-x86_64/install.sh
+++ linux-x86_64/install.sh
@@ -1,4 +1,4 @@
-export DV8_VERSION="0.0.8"
+export DV8_VERSION="0.0.9"
 wget https://github.com/billywhizz/dv8-releases/archive/v$DV8_VERSION.tar.gz
 tar -zxvf v$DV8_VERSION.tar.gz
 cp dv8-releases-$DV8_VERSION/linux-x86_64/dv8 ./
